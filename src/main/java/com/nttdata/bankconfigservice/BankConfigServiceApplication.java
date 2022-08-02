532748d00d760717b79045b9d265791b3c997ed5
Jimmy Willer Maco Elera <jmacoele@nttdata.com>
2022-08-02 11:46:01 -0500

    Seguimos corrigiendo checkstyle.

--- src/main/java/com/nttdata/bankconfigservice/BankConfigServiceApplication.java
+++ src/main/java/com/nttdata/bankconfigservice/BankConfigServiceApplication.java
@@ -3,6 +3,12 @@ package com.nttdata.bankconfigservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * Bank Config Service Application.
+ *
+ * @author jmacoele
+ *
+ */
 
 @EnableConfigServer
 @SpringBootApplication
